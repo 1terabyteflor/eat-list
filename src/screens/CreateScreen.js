7420831b58ea9bd0e619d79ef780a3324c0ee3ca
Florencia Gomez <florenciagomez@MacBook-Pro-de-Florencia.local>
2021-11-08 20:07:08 -0300

    create onTermSubmitHandler

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,33 +1,32 @@
-import React, {useState} from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import Search from '../components/Search';
-import useResults from '../hooks/useResults';
-import RestaurantList from '../components/RestaurantList';
-
-
-const CreateScreen = () => {
-  const [term, setTerm] = useState('');
-  const [searchRestaurants, restaurants, errorMessage] = useResults();
-
-  return (
-    <View style={styles.main}>
-      <Search term={term} onTermChange={setTerm} onTermSubmit={() => searchRestaurants(term)} />
-      {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <ScrollView>
-        <RestaurantList 
-          style={styles.main}
-          restaurants={restaurants}/>
-        </ScrollView>
-    </View>
-  );
-};
+import React, { useState, useCallback } from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import Search from "../components/Search";
+import useResults from "../hooks/useResults";
+import RestaurantList from "../components/RestaurantList";
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1, 
-    alignContent: 'center',
-    justifyContent: 'space-between',
-  }
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "space-between",
+  },
 });
+
+const CreateScreen = () => {
+  const [term, setTerm] = useState("");
+  const [searchRestaurants, restaurants, errorMessage] = useResults();
+
+  const onTermSubmitHandler = useCallback(() => searchRestaurants(term), [term])
+
+  return (
+    <View style={styles.main}>
+      <Search term={term} onTermChange={setTerm} onTermSubmit={onTermSubmitHandler} />
+      {!!errorMessage && <Text>{errorMessage}</Text>}
+      <ScrollView>
+        <RestaurantList style={styles.main} restaurants={restaurants} />
+      </ScrollView>
+    </View>
+  );
+};
 
 export default CreateScreen;
